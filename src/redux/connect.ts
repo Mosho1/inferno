921dce159f40e8510a3513f866179787aff5fd6d
Sharon Rolel <sharonrolel@gmail.com>
2016-10-05 12:12:21 +0300

    change back to imports

--- src/redux/connect.ts
+++ src/redux/connect.ts
@@ -2,12 +2,9 @@ import Component from '../component/es2015';
 import { warning, shallowEqual, wrapActionCreators } from './utils';
 import { isFunction } from '../shared';
 import { createVComponent, IProps } from '../core/shapes';
-/* tslint:disable */
-// TODO: Change to imports and make sure server side / browser tests work
-const hoistStatics = require('hoist-non-inferno-statics');
-const invariant = require('invariant');
-const isPlainObject = require('lodash/isPlainObject');
-/* tslint:enable */
+import hoistStatics from 'hoist-non-inferno-statics';
+import invariant from 'invariant';
+import isPlainObject from 'lodash/isPlainObject';
 
 export interface WrapWithConnect {
 	(WrappedComponent: Component<any, any>): void;
