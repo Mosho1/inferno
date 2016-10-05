1d51232936a7e3c1d0980118b87be55bb187627f
Sharon Rolel <sharonr@tipranks.com>
2016-10-05 20:55:28 +0300

    working on coverage

--- config/compiler.js
+++ config/compiler.js
@@ -1,6 +1,9 @@
 var fs = require('fs');
 var path = require('path');
 var buble = require('buble');
+var babel = require('babel-core');
+var convert = require('convert-source-map');
+var merge = require('merge-source-map');
 
 var nodeVersion = /(?:0\.)?\d+/.exec(process.version)[0];
 var nodeModulesPattern = path.sep === '/' ? /\/node_modules\// : /\\node_modules\\/;
@@ -14,19 +17,29 @@ var options = {
 	}
 };
 
-
 var old = require.extensions['.ts'];
 if (!old) {
 	throw new Error('ts-node/register or equivalent compiler must be used before this compiler.')
 }
+
 require.extensions['.ts'] = function (m, filename) {
 	
 	if ( nodeModulesPattern.test( filename ) ) return old( m, filename );
 	var _compile = m._compile;
-	m._compile = function (code, fileName) {
+	m._compile = function (code, fileName) { 
 		var compiled;
 		try {
-			compiled = buble.transform(code, options);
+			const tsMap = convert.fromMapFileSource(code);
+			const bubleResult = buble.transform(code, options);
+			const tsAndBubleMap = merge(tsMap.sourcemap, bubleResult.map);
+			const transpiledModules = babel.transform(bubleResult.code, {
+				presets: [],
+				plugins: ["transform-es2015-modules-commonjs"]
+			});
+			const finalMap = merge(tsAndBubleMap, transpiledModules.map);
+			const sourceWithoutMap = convert.removeMapFileComments(transpiledModules.code);
+			compiled = sourceWithoutMap + convert.fromObject(finalMap).toComment();
+			// console.log(compiled);
 		} catch (err) {
 			if (err.snippet) {
 				console.log('Error compiling ' + filename + ':\n---');
@@ -37,7 +50,7 @@ require.extensions['.ts'] = function (m, filename) {
 			}
 			throw err;
 		}
-		return _compile.call(this, compiled.code, filename);
+		return _compile.call(this, compiled, filename);
 	}
 	return old(m, filename);
 };
